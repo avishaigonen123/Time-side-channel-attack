217b7d261e3434cb355029eeee186b5ed11dbca9
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2024-06-05 22:17:43 +0300

    strted the keypad

--- Keypad/ESP/src/main.cpp
+++ Keypad/ESP/src/main.cpp
@@ -6,83 +6,147 @@
 #define SUCCESS_PIN 14
 #define PASS_SIZE 4
 
-#define RESET_BUTTON 0x001
-#define ENTER_BUTTON 0x100
-#define INITIALIZE_VALUE 0x909
-#define TOUCH_DELAY 6
+#define ROW_0 GPIO_NUM_15
+#define ROW_1 GPIO_NUM_2
+#define ROW_2 GPIO_NUM_4
+#define ROW_3 GPIO_NUM_16
+#define COL_0 GPIO_NUM_17
+#define COL_1 GPIO_NUM_5
+#define COL_2 GPIO_NUM_18
 
-const uint8_t real_to_fake_keypad[10] = {4, 3, 7, 11, 2, 6, 10, 1, 5 ,9};
+#define RESET_BUTTON 10
+#define ENTER_BUTTON 11
+#define TOUCH_DELAY 5
 
-volatile uint8_t data[4];
 
-uint16_t state = 0;
+uint8_t rowMap(uint8_t num){
+    switch (num)
+    {
+    case 1:
+    case 2:
+    case 3:
+        return ROW_0;
+    case 4:
+    case 5:
+    case 6:
+        return ROW_1;
+    case 7:
+    case 8:
+    case 9:
+        return ROW_2;
+    case RESET_BUTTON:
+    case 0:
+    case ENTER_BUTTON:
+        return ROW_3;
+    default:
+        return -1;
+    }
+}
 
+uint8_t colMap(uint8_t num){
+    switch (num)
+    {
+    case 1:
+    case 4:
+    case 7:
+    case RESET_BUTTON:
+        return COL_0;
+    case 2:
+    case 5:
+    case 8:
+    case 0:
+        return COL_1;
+    case 3:
+    case 6:
+    case 9:
+    case ENTER_BUTTON:
+        return COL_2;
+    default:
+        return -1;
+    }
+}
+
+void writeCol(uint8_t col, uint8_t value) {
+    digitalWrite(COL_0, !value);
+    digitalWrite(COL_1, !value);
+    digitalWrite(COL_2, !value);
+    digitalWrite(col, value);
+}
+
+
+void setColLow(){
+    digitalWrite(COL_0, LOW);
+    digitalWrite(COL_1, LOW);
+    digitalWrite(COL_2, LOW);
+}
+
+void waitToRow(uint8_t row){
+    while(!digitalRead(row));
+}
 
 void relaese(){
-	state = 0;
+    setColLow();
 	delay(TOUCH_DELAY);
+}
+
+bool readyToStart(){
+    return !digitalRead(ROW_0) && !digitalRead(ROW_1) && !digitalRead(ROW_2) && !digitalRead(ROW_3);
 }
 
 void touch(uint8_t num){
 
-	state = 1 << real_to_fake_keypad[num];
-	delay(TOUCH_DELAY);
+	while(readyToStart()); // wait until ready to start, all raws are zero
+    Serial.println("starting touch");
+    writeCol(colMap(num), LOW);
+    Serial.println("wrote LOW");
+    /// set all columns to zero
+
+    setColLow();
+    
+    waitToRow(rowMap(num));
+    Serial.println("got row");
+
+    writeCol(colMap(num), HIGH);
+    Serial.println("wrote col");
+
+    while(readyToStart()); // wait until ready to start, all raws are zero
 }
 
 void reset(){
 	
-	state = RESET_BUTTON;
-	delay(TOUCH_DELAY);
+	touch(RESET_BUTTON);
 }
 
 void enter(){
 	
-	state = ENTER_BUTTON;
-}
-
-void onRec_Callback(int numOfBytes){
-    for(uint8_t i = 0 ; Wire.available() ; i++)
-    {
-        data[i] = Wire.read();
-        //Serial.print(, HEX);
-        //Serial.print(" ");
-    }
-}
-void onReq_Callback(){
-    if(data[0] == 0x5E){
-        Wire.write(0x24);
-        //Serial.println("sent 0x24");
-    }
-    else if(data[0] == 0x5D){
-        Wire.write(0x0);
-        //Serial.println("sent 0x0");
-    }
-    else if(data[0] == 0x00){
-        Wire.write((uint8_t)state);
-        Wire.write(state >> 8);
-    }
-    //Serial.println("\n++++++++++++++++++++++++++++++++++++");
-    data[0] = 0xFF;
+	touch(ENTER_BUTTON);
 }
 
 void setup() {
     Serial.begin(115200);
-    
-    for (uint8_t i = 0; i < 4; i++)
-    {
-        data[i] = 0xFF;
-    }
-    interrupts();
-    Wire.begin(0x5A, SDA, SCL, 400000);
-    Wire.onReceive(onRec_Callback);
-    Wire.onRequest(onReq_Callback);
     pinMode(FAIL_PIN, INPUT);
     pinMode(SUCCESS_PIN, INPUT);
-	Serial.println(clockCyclesPerMicrosecond());
-	relaese();
-	delay(1000);
-}
 
+    pinMode(ROW_0, INPUT);
+    pinMode(ROW_1, INPUT);
+    pinMode(ROW_2, INPUT);
+    pinMode(ROW_3, INPUT);
+    pinMode(COL_0, OUTPUT);
+    pinMode(COL_1, OUTPUT);
+    pinMode(COL_2, OUTPUT);
+}
+void loop(){
+    /*touch(1);
+    relaese();
+    touch(2);
+    relaese();
+    touch(3);
+    relaese();
+    touch(4);
+    relaese();*/
+    Serial.printf("%d %d %d %d\n",digitalRead(ROW_0),digitalRead(ROW_1),digitalRead(ROW_2),digitalRead(ROW_3));
+}
+/*
 uint32_t sendPassword(uint8_t* password)
 {
     
@@ -93,9 +157,7 @@ uint32_t sendPassword(uint8_t* password)
     }
     enter();
 	uint32_t begin_time = micros();
-	noInterrupts();
 	while(!digitalRead(FAIL_PIN) && !digitalRead(SUCCESS_PIN));
-	interrupts();
 	uint32_t end_time = micros();
 
     return  end_time - begin_time;
@@ -129,3 +191,4 @@ void loop() {
     }
 	Serial.println("-----------------------------");
 }
+*/
