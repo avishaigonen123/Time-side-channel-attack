7d9b66758dc18eadae6a094f9f01a2933d0ce333
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2024-07-16 16:08:40 +0300

    ckch
    Co-authored-by: Avishai Gonen <avishaigonen123@users.noreply.github.com>

--- Part3/Arduino/src/main.cpp
+++ Part3/Arduino/src/main.cpp
@@ -3,57 +3,61 @@
 #include "shared/EllipticCurve/ECDSA.h"
 
 // start the software serial, for communication with the ESP32
-// SoftwareSerial SerialESP32(6,7);
-
-// uint32_t privKey = 0x89;
-// uint32_t a = 2;
-// uint32_t b = 3;
-// uint32_t p = 193939;
-// EllipticCurve curve(a,b,p);
-
-// Point point;
-// Point res;
-
-
-// void setup(){
-//     SerialESP32.begin(115200);
-//     Serial.begin(115200);
-// }
-
-// void loop(){
-//     if(SerialESP32.available()){
-//         point.x = SerialESP32.parseInt();
-//         SerialESP32.read(); // dump the space
-//         point.y = SerialESP32.parseInt();
-// 		SerialESP32.read(); // dump the newline
-//         point.print(&Serial);
-//         // calculate the point, and send via the serial to the server
-// 		Point res = curve.EllipticCurveCalcPoint(point, privKey);
-//         res.print(&SerialESP32);
-// 		res.print(&Serial);
-//         // dump the rest
-//         byte* garbeg = new byte[SerialESP32.available()];
-//         SerialESP32.readBytes(garbeg, SerialESP32.available());
-//         delete[] garbeg;
-//         garbeg = nullptr;
-//     }
-
-// }
+SoftwareSerial SerialESP32(6,7);
 
 ECDSA ecdsa;
+uint32_t privKey = 0xBA;
 
+uint32_t hash = 1234;
+
+// Debugging helper to print signature
+void printSignature(ECDSA_sig_t sig, Stream &serial) {
+    serial.print(sig.r);
+    serial.print(" ");
+    serial.println(sig.s);
+}
+
+// dump garbage
+void flush(Stream &serial){
+    byte* garbeg = new byte[serial.available()];
+    serial.readBytes(garbeg, serial.available());
+    delete[] garbeg;
+    garbeg = nullptr;
+}
+
+// start serials
 void setup(){
+    SerialESP32.begin(115200);
     Serial.begin(115200);
-    randomSeed(analogRead(0));
-    ECDSA_keypair_t keys = ecdsa.make_keypair();
-    uint32_t hash = 1234;
-    for (size_t i = 0; i < 20; i++){
-        ECDSA_sig_t sig = ecdsa.sign(keys.privKey, hash);
-        Serial.println(ecdsa.verify(keys.Q, sig, hash));
-    }
 }
 
 void loop(){
-    
+    if(SerialESP32.available() && SerialESP32.read() == 'S'){
+        ECDSA_sig_t sig = ecdsa.sign(privKey, hash);
+        printSignature(sig, SerialESP32);
+        // printSignature(sig, Serial);
+        // dump the rest
+        flush(SerialESP32);
+        // flush(Serial);
+    }
 }
+
+// ECDSA ecdsa;
+// uint32_t hash = 1234;
+
+// void setup(){
+//     Serial.begin(115200);
+//     randomSeed(analogRead(0));
+//     ECDSA_keypair_t keys = ecdsa.make_keypair();
+
+//     for (size_t i = 0; i < 20; i++){
+//         ECDSA_sig_t sig = ecdsa.sign(keys.privKey, hash);
+//         Serial.println(ecdsa.verify(keys.Q, sig, hash));
+//     }
+// }
+
+// void loop(){
+
+    
+// }
 
